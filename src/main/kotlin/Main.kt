dfbea85647967ec05ce717d1122e067218b7e381
Michiel Idema <michielidema@outlook.com>
2024-02-11 23:50:06 +0100

    temp.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,8 @@ import main.kotlin.lobby.Lobby
 import network.PlayerWebsocket
 
 fun main(args: Array<String>){
+    println("Running on Java version: ${System.getProperty("java.version")}")
+
     PlayerWebsocket().also {
         it.initialize()
     }
